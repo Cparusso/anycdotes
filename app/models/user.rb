67c2ca3f0a52b87e1a380ef2f3ce129180660077
github username <github email address>
2018-10-02 09:44:38 -0400

    Added validatons to the User forms.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   has_many :stories
   has_many :locations, through: :stories
+
+  validates :username,
+            :presence => true,
+            :uniqueness => true
+
+  validates :username,
+            :presence => true,
+
 end
